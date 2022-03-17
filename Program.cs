d80dec14f414b3c980d53025e19f63e6112cbe69
DocS1887 <docs1887googlemail.com>
2022-03-17 18:07:16 +0100

    create project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SchleifenInAnwendungen_vsc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
